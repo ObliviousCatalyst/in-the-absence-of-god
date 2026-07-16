bfc7c155f987b4d0284082cb94e2bca7bba1fbd4
ObliviousCatalyst <jackcarletonriley@gmail.com>
2026-07-16 11:41:57 -0400

    added interact action

--- Assets/scripts/controller_V3.cs
+++ Assets/scripts/controller_V3.cs
@@ -5,22 +5,24 @@ public class controller_V3 : MonoBehaviour {
 	public CharacterController controller;
 	public float moveSpd = 10f, rotSpd = 5f;
 	float rotY;
-	void Start() {
+	void Start () {
 		controller = GetComponent<CharacterController>();
 	}
 
-	void Update() {
-		
-	}
-
-	public void move(Vector2 movementVector) {
+	public void move (Vector2 movementVector) {
 		Vector3 movement = transform.forward * movementVector.y + transform.right * movementVector.x;
 		movement = movement * moveSpd * Time.deltaTime;
 		controller.Move(movement);
 	}
 
-	public void rotate(Vector2 lookVector) {
+	public void rotate (Vector2 lookVector) {
 		rotY += lookVector.x * rotSpd * Time.deltaTime;
 		transform.localRotation = Quaternion.Euler(0,rotY,0);
+	}
+
+	public void interact (bool playerInteract) {
+		if (playerInteract) {
+			// do some shit
+		}
 	}
 }
--- Assets/scripts/input_handler.cs
+++ Assets/scripts/input_handler.cs
@@ -3,11 +3,12 @@ using UnityEngine.InputSystem;
 
 public class input_handler : MonoBehaviour {
 	public controller_V3 V3controller;
-	InputAction moveAct, lookAct;
+	InputAction moveAct, lookAct, interAct;
 
 	void Start() {
 		moveAct = InputSystem.actions.FindAction("Move");
 		lookAct = InputSystem.actions.FindAction("Look");
+		interAct = InputSystem.actions.FindAction("Interact");
 		Cursor.visible = false;
 		Cursor.lockState = CursorLockMode.Locked;
 	}
@@ -17,5 +18,7 @@ public class input_handler : MonoBehaviour {
 		V3controller.move(movementVector);
 		Vector2 lookVector = lookAct.ReadValue<Vector2>();
 		V3controller.rotate(lookVector);
+		bool playerInteract = interAct.ReadValue<bool>();
+		V3controller.interact(playerInteract);
 	}
 }
