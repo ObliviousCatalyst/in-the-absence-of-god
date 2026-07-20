3ebce9362d80eee839be5e1a1020043e2db3954b
ObliviousCatalyst <jackcarletonriley@gmail.com>
2026-07-20 12:56:19 -0400

    fixed move/rotation speed issue

--- Assets/scripts/controller_V3.cs
+++ Assets/scripts/controller_V3.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 public class controller_V3 : MonoBehaviour {
 	public CharacterController controller;
-	public float moveSpd = 10f, rotSpd = 5f;
+	public float moveSpd = 10f, rotSpd = 25;
 	float rotY;
 	void Start () {
 		controller = GetComponent<CharacterController>();
