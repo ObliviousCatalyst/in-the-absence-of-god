43e3bddaacff8f20893f6f58482502f7e9bf5ec7
ObliviousCatalyst <jackcarletonriley@gmail.com>
2026-07-30 09:49:21 -0400

    finalized build

--- Assets/menu stuff/respawn.cs
+++ Assets/menu stuff/respawn.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 public class respawn : MonoBehaviour {
 	[SerializeField] Button btn;
 	void Start() {
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.None;
 		btn.onClick.AddListener(() => {
 			SceneManager.LoadScene("forest");
 		});
